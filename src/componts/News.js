764b05f6a6eb5863682dc0f4000f949ad7fd53e0
ganesh <ganeshparjaptm5@gmail.com>
2023-02-20 15:50:41 +0530

    add card list by bootstrap

--- src/componts/News.js
+++ src/componts/News.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export default class news extends Component {
   render() {
     return (
-      <div>
-        thi si new componts
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className="container my-3">
+        <h3>News Monkey Headlings today</h3>
+        <div className="row my-3" >
+          <div className="col-md-4">
+            <NewsItem title="my title" discription="my discropation" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" discription="my discropation" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" discription="my discropation" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
--- src/componts/NewsItem.js
+++ src/componts/NewsItem.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, discription } = this.props;
     return (
       <div>
-        this is newsItem based componets
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="https://www.indy100.com/media-library/18-years-of-satellite-imagery-shows-receding-of-thwaites-glacier.jpg?id=33056526&width=1200&height=600&coordinates=0%2C16%2C0%2C154" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{discription}</p>
+            <a href="/newsitem" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
