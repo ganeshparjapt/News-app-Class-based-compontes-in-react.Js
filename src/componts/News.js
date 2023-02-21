4600a6b2f14b337593a6da181c15718e7fe9e50e
ganesh <ganeshparjaptm5@gmail.com>
2023-02-21 12:03:27 +0530

    render set state Api and add button next and prieview

--- src/componts/News.js
+++ src/componts/News.js
@@ -79,19 +79,44 @@ export default class news extends Component {
      
   }
 }
+async componentDidMount(){
+  console.log("did mount")
+  let url = "https://newsapi.org/v2/top-headlines?country=id&apiKey=f8264201c0a04e829cc23fb70753cd26&page=1";
+  let data = await fetch(url);
+
+  let parseDate = await(data.json())
+  console.log(parseDate)
+  this.setState({articles: parseDate.articles})
+}
+handleNextClick =() =>{
+  console.log("click next")
+}
+handlePreviewClick = ()=>{
+  console.log("click preview")
+}
    
   render() {
+    console.log("did ")
+
     return (
+      
       <div className="container my-3">
         <h3>News Monkey Headlings today</h3>
         <div className="row my-3" >
           {this.state.articles.map((element) => {
             return <div className="col-md-4">
-            <NewsItem title={element.title.slice(0,45)} descc={element.description.slice(0,90)} imgUrl={element.urlToImage} newsUrl={element.url} />
+            <NewsItem title={element.title ? element.title.slice(0,45):""} descc={
+              element.title ? element.description.slice(0,90):""} imgUrl={element.urlToImage  } newsUrl={element.url}
+             />
           </div>
           })}
          
          
+        </div>
+        < div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" class="btn btn-dark mx-3"onClick={this.handlePreviewClick}>&larr; Preview</button>
+        <button type="button" class="btn btn-dark"onClick={this.handleNextClick}>next news &rarr;</button>
+
         </div>
       </div>
     );
